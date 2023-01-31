5345b6a3233529e5b205363254fd6aa4b3f608bc
cesar lucumi <clucumi.94@gmail.com>
2023-01-30 22:52:24 -0500

    Build context and Scaffold

--- counter_app/lib/screens/home_screen.dart
+++ counter_app/lib/screens/home_screen.dart
@@ -8,9 +8,11 @@ class HomeScreen extends StatelessWidget{
 
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('HomeScreen'),
+  Widget build(BuildContext context) { // build context: sirve para saber el contexto en el que el widget esta siendo construido
+    return const Scaffold( //Canvas donde se ponen los widgets
+      body: Center(
+        child: Text('HomeScreen'),
+      ),
     );
   }
 
