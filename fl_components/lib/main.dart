947ea22f3f181196d3d0536eca8ba66a65ee2cb8
cesar lucumi <clucumi.94@gmail.com>
2023-02-11 20:36:23 -0500

    Creando AppRouter para organizar rutas y navegacion

--- fl_components/lib/main.dart
+++ fl_components/lib/main.dart
@@ -1,8 +1,5 @@
+import 'package:fl_components/router/app_routes.dart';
 import 'package:flutter/material.dart';
-
-import 'screens/screens.dart';
-
-
 
 void main() => runApp(const MyApp());
 
@@ -15,20 +12,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       // home: const CardScreen(),
-      initialRoute: 'home',
-      routes:{
-        'home'      : (BuildContext context) => const HomeScreen(),
-        'listview1' : (BuildContext context) => const Listview1Screen(),
-        'listview2' : (BuildContext context) => const Listview2Screen(),
-        'alert'     : (BuildContext context) => const AlertScreen(),
-        'card'      : (BuildContext context) => const CardScreen(),
-      },
-      onGenerateRoute:(settings) {
-        print(settings);
-        return  MaterialPageRoute(
-          builder: (context) => const AlertScreen(),
-        );
-      },
+      initialRoute: AppRoutes.initialRoute,
+      routes:AppRoutes.routes,
+      onGenerateRoute:AppRoutes.onGenerateRoute
     );
   }
 }
