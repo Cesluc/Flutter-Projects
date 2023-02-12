5bbb74fed5948e87f7c9afb2f6b1a4b7fa18b929
cesar lucumi <clucumi.94@gmail.com>
2023-02-12 13:26:45 -0500

    Ajustar el tamaño de la imagen

--- fl_components/lib/widgets/custom_card_type2.dart
+++ fl_components/lib/widgets/custom_card_type2.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
@@ -6,14 +7,28 @@ class CustomCardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 10,
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18)
+      ) ,
+      elevation: 30,
+      shadowColor: AppTheme.primary.withOpacity(0.5),
       child: Column(
-        children: const [
+        children:  [
 
-          FadeInImage(
+          const FadeInImage(
             image:  NetworkImage('https://img.freepik.com/free-vector/nature-scene-with-river-hills-forest-mountain-landscape-flat-cartoon-style-illustration_1150-37326.jpg?w=2000'), placeholder: AssetImage('assets/jar-loading.gif'),
-          )
+            width: double.infinity,
+            height: 260,
+            fit: BoxFit.cover,
+            fadeInDuration:  Duration(milliseconds: 300) ,
+          ),
 
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+            child: const Text('Un hermoso paisaje')
+          )
         ],
       ),
     );
