5453c3498895e14267499f92047212f2baf020b1
cesar lucumi <clucumi.94@gmail.com>
2023-02-13 12:41:43 -0500

    Mostrando la alerta y añadiendo estilos a la tarjeta

--- fl_components/lib/screens/alert_screen.dart
+++ fl_components/lib/screens/alert_screen.dart
@@ -3,6 +3,36 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
+
+  void displayDialog(BuildContext context){
+
+    showDialog(
+      barrierDismissible: false,
+      context: context, 
+      builder: (context){
+        return AlertDialog(
+          elevation: 5,
+          title:  const Text ('Titulo'),
+          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(15)),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+               Text('Este es el contenido de una alerta'),
+               SizedBox(height: 10),
+               FlutterLogo(size: 100,)
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context), 
+              child: const Text('Cancelar')
+            )
+          ],
+        );
+      }
+    );
+
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -14,9 +44,7 @@ class AlertScreen extends StatelessWidget {
           //   shape: const StadiumBorder(),
           //   elevation: 0
           // ),
-          onPressed: () {
-            
-          }, 
+          onPressed: () => displayDialog(context), 
           child: const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
             child: Text('Mostrar alerta', style: TextStyle(fontSize: 20),),
