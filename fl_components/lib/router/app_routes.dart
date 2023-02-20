21826085902f02393ef0c0610dc9d2e75882fcc1
cesar lucumi <clucumi.94@gmail.com>
2023-02-20 12:18:21 -0500

    Animated containter added with duration and curves to make the animations

--- fl_components/lib/router/app_routes.dart
+++ fl_components/lib/router/app_routes.dart
@@ -9,7 +9,7 @@ class AppRoutes {
 
   static final menuOptions = <MenuOption>[
     //TODO: borrar Home
-    MenuOption(route: 'home',  name: 'Home Screen', screen: const HomeScreen(), icon: Icons.home_filled),
+    // MenuOption(route: 'home',  name: 'Home Screen', screen: const HomeScreen(), icon: Icons.home_filled),
     MenuOption(route: 'listview1',  name: 'Listview tipo 1', screen: const Listview1Screen(), icon: Icons.list_alt),
     MenuOption(route: 'listview2',  name: 'Listview tipo 2', screen: const Listview2Screen(), icon: Icons.list),
     MenuOption(route: 'alert',  name: 'Alertas - Alerts', screen: const AlertScreen(), icon: Icons.add_alert_outlined),
@@ -21,6 +21,8 @@ class AppRoutes {
   static Map<String, Widget Function(BuildContext)> getAppRoutes(){
 
     Map<String, Widget Function(BuildContext)> appRoutes = {};
+    appRoutes.addAll({'home': (BuildContext context) => const HomeScreen()});
+
 
     for(final option in menuOptions){
       appRoutes.addAll({option.route: (BuildContext context) => option.screen});
