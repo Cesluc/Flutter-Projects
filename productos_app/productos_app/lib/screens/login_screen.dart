c3e3a732c74dc5def076ff9fc6cff2eb61470a46
cesar lucumi <clucumi.94@gmail.com>
2023-03-05 15:15:53 -0500

    Reutilizando los estilos de inputs

--- productos_app/productos_app/lib/screens/login_screen.dart
+++ productos_app/productos_app/lib/screens/login_screen.dart
@@ -1,38 +1,105 @@
 import 'package:flutter/material.dart';
 
+import '../ui/input_decorations.dart';
 import '../widgets/widgets.dart';
 
-
 class LoginScreen extends StatelessWidget {
-
-   
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: AuthBackground(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: 250,),
-              CardContainer(
-                child: Column(
-                  children: [
-
-                    SizedBox(height: 10,),
-                    Text('Login', style: Theme.of(context).textTheme.headlineMedium,),
-                    SizedBox(height: 30,),
-
-                    Text('formulario')
-
-
-                  ],
-                ),
-              )
-
-            ],
+    return Scaffold(
+        body: AuthBackground(
+            child: SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 250,
           ),
-        )
-      )
+          CardContainer(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Login',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                _LoginForm(),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Text(
+            'Create new account',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 50,
+          )
+        ],
+      ),
+    )));
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        //TODO: Mantener la referencia al KEY
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: 'project-r@vipa.com',
+                  labelText: 'Email Address',
+                  prefixIcon: Icons.alternate_email_sharp),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: '********',
+                  labelText: 'Password',
+                  prefixIcon: Icons.lock_outline),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            MaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: const Text(
+                    'Log in',
+                    style: TextStyle(color: Colors.white),
+                  )),
+              onPressed: () {
+                //TODO: login Form
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
