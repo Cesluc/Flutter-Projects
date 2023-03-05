c9b9b850d0e013cdbf73dc963edd713f6b4ee6b2
cesar lucumi <clucumi.94@gmail.com>
2023-03-05 00:23:53 -0500

    Fondo del login

---
+++ productos_app/productos_app/lib/screens/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/widgets.dart';
+
+
+class LoginScreen extends StatelessWidget {
+
+   
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: AuthBackground()
+    );
+  }
+}
