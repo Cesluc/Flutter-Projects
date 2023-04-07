aa0b972a06bbaa1ca56ab831d236a0627ee48d7f
cesar lucumi <clucumi.94@gmail.com>
2023-04-07 00:54:21 -0500

    Pantalla de producto parte 2

--- productos_app/productos_app/lib/screens/product_screen.dart
+++ productos_app/productos_app/lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -6,18 +7,119 @@ class ProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             Stack(
               children: [
-                ProductImage()
+                const ProductImage(),
+                Positioned(
+                  top: 60,
+                  left: 20,
+                  child: IconButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    icon: const Icon(
+                      Icons.arrow_back_ios_new,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 60,
+                  right: 30,
+                  child: IconButton(
+                    onPressed: () {
+                      //TODO: Camara o galeria
+                    },
+                    icon: const Icon(
+                      Icons.camera_alt_outlined,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
               ],
+            ),
+            const _ProductForm(),
+            const SizedBox(
+              height: 100,
             )
           ],
         ),
-      )
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //TODO Guardar producto
+        },
+        child: const Icon(Icons.save_outlined),
+      ),
     );
   }
+}
+
+class _ProductForm extends StatelessWidget {
+  const _ProductForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+        decoration: _buildBoxDecoration(),
+        child: Form(
+            child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: 'Nombre de producto', labelText: 'Nombre:'),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: '\$150', labelText: 'Precio:'),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SwitchListTile.adaptive(
+                value: true,
+                title: const Text('Disponible'),
+                activeColor: Colors.indigo,
+                onChanged: (value) {
+                  //todo: Pending
+                }),
+            const SizedBox(
+              height: 30,
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.only(
+              bottomRight: Radius.circular(25),
+              bottomLeft: Radius.circular(25)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.05),
+              offset: const Offset(0, 5),
+              blurRadius: 5,
+            )
+          ]);
 }
