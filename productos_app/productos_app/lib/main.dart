5d0794ba1824e8dfa442e46956ed32fec448fc60
cesar lucumi <clucumi.94@gmail.com>
2023-04-07 00:23:44 -0500

    Pantalla de edicion producto - 221

--- productos_app/productos_app/lib/main.dart
+++ productos_app/productos_app/lib/main.dart
@@ -12,23 +12,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Products App',
-      initialRoute: 'home',
+      initialRoute: 'product',
       routes: {
-        'login':(_) => LoginScreen(),
-        'home':(_) => HomeScreen(),
-
+        'login': (_) => LoginScreen(),
+        'home': (_) => HomeScreen(),
+        'product': (_) => ProductScreen(),
       },
       theme: ThemeData.light().copyWith(
-        scaffoldBackgroundColor: Colors.grey[300],
-        appBarTheme: const AppBarTheme(
-          elevation: 0,
-          color: Colors.indigo
-        ),
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: Colors.indigo,
-          elevation: 0
-        )
-      ),
+          scaffoldBackgroundColor: Colors.grey[300],
+          appBarTheme: const AppBarTheme(elevation: 0, color: Colors.indigo),
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: Colors.indigo, elevation: 0)),
     );
   }
 }
--- productos_app/productos_app/lib/screens/home_screen.dart
+++ productos_app/productos_app/lib/screens/home_screen.dart
@@ -2,21 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
-   
-  
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Productos'),
       ),
       body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) => const ProductCard()
-      ), 
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) => GestureDetector(
+              onTap: () => Navigator.pushNamed(context, 'product'),
+              child: const ProductCard())),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed:() {}, 
+        onPressed: () {},
       ),
     );
   }
